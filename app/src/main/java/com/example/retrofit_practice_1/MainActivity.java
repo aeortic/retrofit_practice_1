9a0e3b1bb69f27a46597b7bf23f49de5c675967f
Chris Okerberg <seminative@gmail.com>
2020-01-23 23:37:14 -0500

    Removed unused network, repurposed adapter

--- app/src/main/java/com/example/retrofit_practice_1/MainActivity.java
+++ app/src/main/java/com/example/retrofit_practice_1/MainActivity.java
@@ -13,13 +13,9 @@ import android.widget.Toast;
 import com.example.retrofit_practice_1.adapter.CustomAdapter;
 import com.example.retrofit_practice_1.model.MovieDatabase;
 import com.example.retrofit_practice_1.model.RetroPhoto;
-import com.example.retrofit_practice_1.network.GetDataService;
 import com.example.retrofit_practice_1.network.GetMovieService;
-import com.example.retrofit_practice_1.network.RetrofitClientInstance;
 import com.example.retrofit_practice_1.network.RetrofitMovieInstance;
-import com.google.gson.JsonElement;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -31,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     private CustomAdapter adapter;
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
-    private Call<List<RetroPhoto>> call;
     private Call<MovieDatabase> movieCall;
 
     @Override
@@ -42,50 +37,38 @@ public class MainActivity extends AppCompatActivity {
         progressBar = findViewById(R.id.indeterminateBar);
         progressBar.setVisibility(View.VISIBLE);
 
-        final TextView baconIdView = findViewById(R.id.baconIdView);
-
-        /* Create handler for the Retrofit interface */
-        GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
-        call = service.getAllPhotos();
-        call.enqueue(new Callback<List<RetroPhoto>>() {
-            @Override
-            public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
-                progressBar.setVisibility(View.INVISIBLE);
-                /* generate the list here */
-                generateDataList(response.body());
-            }
-
-            @Override
-            public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
-                progressBar.setVisibility(View.INVISIBLE);
-                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-            }
-        });
+        final TextView filmCareerTitleView = findViewById(R.id.filmCareerTitleView);
 
         GetMovieService movieService = RetrofitMovieInstance.getRetrofitInstance().create(GetMovieService.class);
         movieCall = movieService.getSmallDataSet();
         movieCall.enqueue(new Callback<MovieDatabase>() {
             @Override
             public void onResponse(Call<MovieDatabase> call, Response<MovieDatabase> response) {
+                progressBar.setVisibility(View.INVISIBLE);
                 HashMap<String, Integer> actors = response.body().getActors();
                 Integer baconId = actors.get("Kevin Bacon");
 
-                baconIdView.setText("Kevin Bacon's id is "+baconId);
+                Toast.makeText(MainActivity.this, "Kevin Bacon's id is "+baconId, Toast.LENGTH_SHORT).show();
+
+                filmCareerTitleView.setText("Kevin Bacon's career consists of these films:");
 
                 List<String> filmCareer = response.body().getMoviesByActorId(baconId);
+
+                generateDataList(filmCareer);
 
             }
 
             @Override
             public void onFailure(Call<MovieDatabase> call, Throwable t) {
+                progressBar.setVisibility(View.INVISIBLE);
                 Toast.makeText(MainActivity.this, "Something went wrong with the movie call", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    private void generateDataList(List<RetroPhoto> photoList) {
+    private void generateDataList(List<String> filmCareer) {
         recyclerView = findViewById(R.id.customRecyclerView);
-        adapter = new CustomAdapter(this, photoList);
+        adapter = new CustomAdapter(this, filmCareer);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
@@ -94,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        call.cancel();
         movieCall.cancel();
     }
 }
